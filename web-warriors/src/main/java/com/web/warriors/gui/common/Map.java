8b1296c7ec9ec6477570212df4bbc129fdc040fd
Puchinni <utymaster22@gmail.com>
2024-06-07 14:25:53 +0200

    pictures added

--- web-warriors/src/main/java/com/web/warriors/gui/common/Map.java
+++ web-warriors/src/main/java/com/web/warriors/gui/common/Map.java
@@ -6,8 +6,12 @@ import com.web.warriors.game.objects.Hostage;
 import com.web.warriors.game.objects.Player;
 import com.web.warriors.game.objects.Wall;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.TimerTask;
 import java.util.Vector;
@@ -22,6 +26,11 @@ public class Map extends JPanel {
     private Collection<Bullet> bullets;
     private Collection<Wall> walls;
     private Player player = null;
+    String currentDirectory = System.getProperty("user.dir") + "/web-warriors/src/main/resources/images";
+    BufferedImage ct;
+    BufferedImage t;
+    BufferedImage dead;
+    BufferedImage hostageImg;
 
     public Map(GameEngine gameEngine) {
         setPreferredSize(new Dimension(cellSize * mapSize, cellSize * mapSize));
@@ -31,6 +40,15 @@ public class Map extends JPanel {
         this.walls = gameEngine.getWalls();
         this.hostages = gameEngine.getHostages();
         this.bullets = gameEngine.getBullets();
+        try {
+        this.ct = ImageIO.read(new File(currentDirectory + "/ct.png"));
+        this.t = ImageIO.read(new File(currentDirectory + "/t.png"));
+        this.dead = ImageIO.read(new File(currentDirectory + "/dead.png"));
+        this.hostageImg = ImageIO.read(new File(currentDirectory + "/hostage.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // addKeyListener(this);
 
@@ -51,10 +69,10 @@ public class Map extends JPanel {
     }
 
     private void paintPlayer(Graphics g, Player player, int size, Color color) {
-        g.setColor(color);
+        
         int centerX = (player.getX() * cellSize / 6) + 5;
         int centerY = (player.getY() * cellSize / 6) + 5;
-
+        
         double angleRad = player.getAngle();
         int x1 = centerX + (int) ((size + 3) * Math.cos(angleRad));
         int y1 = centerY - (int) ((size + 3) * Math.sin(angleRad));
@@ -65,17 +83,52 @@ public class Map extends JPanel {
 
         int[] PointsX = { x1, x2, x3 };
         int[] PointsY = { y1, y2, y3 };
-        g.fillPolygon(PointsX, PointsY, 3);
+        int xG = (PointsX[0] + PointsX[1] + PointsX[2]) / 3 - 10;
+        int yG = (PointsY[0] + PointsY[1] + PointsY[2]) / 3 - 10;
+
+        if (color == Color.BLUE) {
+            g.drawImage(ct, xG, yG, 35, 35, null);
+        }
+        else if (color == Color.RED) {
+            g.drawImage(t, xG, yG, 35, 35, null);
+        }
+        else if(color == Color.GRAY) {
+            g.drawImage(dead, xG, yG, 35, 35, null);
+        }
+        //g.setColor(color);
+        //g.fillPolygon(PointsX, PointsY, 3);
+
+
+        
+        
+
+
+        // g.setColor(color);
+        // int centerX = (player.getX() * cellSize / 6) + 5;
+        // int centerY = (player.getY() * cellSize / 6) + 5;
+
+        // double angleRad = player.getAngle();
+        // int x1 = centerX + (int) ((size + 3) * Math.cos(angleRad));
+        // int y1 = centerY - (int) ((size + 3) * Math.sin(angleRad));
+        // int x2 = centerX + (int) (size * Math.cos(angleRad + 2 * Math.PI / 3));
+        // int y2 = centerY - (int) (size * Math.sin(angleRad + 2 * Math.PI / 3));
+        // int x3 = centerX + (int) (size * Math.cos(angleRad - 2 * Math.PI / 3));
+        // int y3 = centerY - (int) (size * Math.sin(angleRad - 2 * Math.PI / 3));
+
+        // int[] PointsX = { x1, x2, x3 };
+        // int[] PointsY = { y1, y2, y3 };
+        // g.fillPolygon(PointsX, PointsY, 3);
 
         Hostage hostage = player.getHostage();
         if (hostage != null) {
             // TODO change hostage painting to a different method
-            g.setColor(Color.GREEN);
+            // g.setColor(Color.GREEN);
             int radius = cellSize / 2;
 
             int hostageCenterX = player.getX() * cellSize / 6 + radius;
             int hostageCenterY = player.getY() * cellSize / 6 + radius;
-            g.fillOval(hostageCenterX - radius, hostageCenterY - radius, cellSize / 2, cellSize / 2);
+            // g.fillOval(hostageCenterX - radius, hostageCenterY - radius, cellSize / 2, cellSize / 2);
+            g.drawImage(hostageImg, hostageCenterX - radius, hostageCenterY - radius, 25, 25, null);
         }
     }
 
@@ -125,7 +178,11 @@ public class Map extends JPanel {
 
             int centerX = hostage.getX() * cellSize / 6 + radius;
             int centerY = hostage.getY() * cellSize / 6 + radius;
-            g.fillOval(centerX - radius, centerY - radius, cellSize, cellSize);
+            // g.fillOval(centerX - radius, centerY - radius, cellSize, cellSize);
+            g.drawImage(hostageImg, centerX - radius, centerY - radius, 35, 35, null);
+
+
+            
         }
 
         for (Bullet bullet : bullets) {
